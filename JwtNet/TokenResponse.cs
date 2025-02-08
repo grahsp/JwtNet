89daa90d5ae67663cbf84806f793d2638dc54349
grahsp <pontus.grahsp@gmail.com>
2025-02-08 22:28:04 +0100

    feat(service): add a token response object

---
+++ JwtNet/TokenResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace JwtNet
+{
+    public class TokenResponse
+    {
+        public string AccessToken { get; }
+        public string RefreshToken { get; }
+        public DateTime Expires { get; }
+
+        public TokenResponse(string accessToken, string refreshToken, DateTime expires)
+        {
+            AccessToken = accessToken;
+            RefreshToken = refreshToken;
+            Expires = expires;
+        }
+    }
+}
