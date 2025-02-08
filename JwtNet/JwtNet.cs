ae66b1e0a5e1fbace0c75da7822b4d067e5b7c77
grahsp <pontus.grahsp@gmail.com>
2025-02-08 19:47:01 +0100

    feat(service): create service with method to create a token

---
+++ JwtNet/JwtNet.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace JwtNet
+{
+    public class JwtNet
+    {
+        private readonly JwtNetOptions _options;
+        private readonly ISigningStrategy _signingStrategy;
+
+        public JwtNet(IOptions<JwtNetOptions> options)
+        {
+            _options = options.Value;
+            _signingStrategy = _options.SigningStrategy;
+        }
+
+        public string CreateToken(IEnumerable<Claim> claims)
+        {
+            var jwtToken = new JwtSecurityToken(
+                issuer: _options.Issuer,
+                audience: _options.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_options.ExpiresInMinutes),
+                signingCredentials: _signingStrategy.GetSigningCredentials()
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtToken);
+        }
+    }
+}
