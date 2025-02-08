079869c3fd1ce83d557cdea0c79bb4f8185b8ff2
grahsp <pontus.grahsp@gmail.com>
2025-02-08 19:46:41 +0100

    feat(options): create options object to configure jwt service

---
+++ JwtNet/JwtNetOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace JwtNet
+{
+    public class JwtNetOptions
+    {
+        public required ISigningStrategy SigningStrategy { get; set; }
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
+        public int ExpiresInMinutes { get; set; } = 30;
+    }
+}
