f9580d425afb96a4681b54a88ef9a2fa13d8ebd0
grahsp <pontus.grahsp@gmail.com>
2025-02-09 13:58:31 +0100

    feat(builder): add extension methods for services for streamlined approach

---
+++ JwtNet/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace JwtNet
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static JwtNetBuilder AddJwtNet(this IServiceCollection services)
+        {
+            return new JwtNetBuilder(services);
+        }
+
+        public static JwtNetBuilder AddJwtNet(this IServiceCollection services, Action<JwtNetOptions> options)
+        {
+            services.Configure(options);
+            services.AddSingleton<JwtNet>();
+
+            return new JwtNetBuilder(services);
+        }
+    }
+}
